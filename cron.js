a6ae703fb52db539ff42d4d4f1da90238113ad3d
AvishekDe <avishekde.iitr@gmail.com>
2014-03-01 13:06:36 +0530

    Commit after removing deleted files and adding two new files

--- cron.js
+++ cron.js
@@ -1,1 +1,41 @@
-var parser = require("./parser.js");
+var fs = require('fs');
+
+fs.writeFile('status.txt', 'CURRENT STATUS OF SDS WEB APPS', function (err) {
+  if (err) throw err;
+});
+fs.appendFile('status.txt', '\n-----------------------------------------\n' , function (err){
+	if(err) throw err;
+});
+
+var parser = require('./parser.js');
+var n=parser.value;
+var arr=parser.expArr;
+var url = "" , name = "";
+var request = require('request');
+var i;
+
+for(i=0;i<n;i++)
+{
+	if(arr[i][1]) // For Apps with URL
+	{
+		url = arr[i][2];
+		name = arr[i][0];
+		request('http://'+url, function (error, response, body) {
+		  if (!error && response.statusCode == 200) {
+			    fs.appendFile('status.txt' , (i+1)+'.\t'+name+'\t 200 OK \n' , function (err){
+			    	if (err) throw err;
+			    });
+		  }
+		   else
+		   {
+		   		fs.appendFile('status.txt' , (i+1)+'.\t'+name+'\t 404 NOT FOUND \n' , function (err){
+		    	if (err) throw err;
+		    	});
+		   }
+		});
+	}
+	else // For DC++
+	{
+
+	}
+}
--- parser.js
+++ parser.js
@@ -1,9 +1,35 @@
-module.export = {
-	parser:function(txt)
+var fs = require('fs');
+var ini = require('ini');
+var iniReader = require('inireader');
+var parser = new iniReader.IniReader();
+parser.load('./config.ini');
+var sec;
+var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+var n = 11;
+var dataArr = new Array(n);
+var i=0,j;
+for(sec in config)
+{
+	var str = sec.toString();
+	secm = parser.param(str); // obtains block
+	if(secm.hasOwnProperty('url'))
 	{
-
+		dataArr[i] = new Array(3);
+		dataArr[i][0] = parser.param(str+'.tag');
+		dataArr[i][1] = "true";
+		dataArr[i][2] = parser.param(str+'.url');
 	}
-	defaults:auto = {
-		user-agent = "curl"
+	else
+	{
+		dataArr[i] = new Array(4);
+		dataArr[i][0] = parser.param(str+'.tag');
+		dataArr[i][1] = "false";
+		dataArr[i][2] = parser.param(str+'.port');
+		dataArr[i][3] = parser.param(str+'.host');
 	}
+	i++;
+}
+module.exports = {
+	expArr : dataArr,
+	value : n 
 }
