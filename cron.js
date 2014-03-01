7b61380a4d834049740ea4f63663128fd7ffe48c
AvishekDe <avishekde.iitr@gmail.com>
2014-03-02 02:32:54 +0530

    Added status detection for DC++ Updated config.sample.ini to include more Web Apps

--- cron.js
+++ cron.js
@@ -1,3 +1,4 @@
+var net = require('net');
 var fs = require('fs'),
     ini = require('./parser'),
     request = require("request"),
@@ -10,15 +11,34 @@ var count = Object.keys(ini).length;//Number of sections in our INI file
 var cb = _.after(count, function(){
     fs.writeFile('output.json', JSON.stringify(output));
 });
-for(var i in ini){
+for(var i in ini)
+{
     var config = ini[i];
     console.log(config.name);
-    if(config.url){
+    if(config.url)
+    {
         requester(config.url, config.name, cb);
     }
-    else{
+    else
+    {
         //try connecting to the host,port and call cb once its done
-        cb();
+        var client = net.connect({port: 411 , host:'dc.sdslabs.co.in'}, function(err , res , body) 
+        {
+            //'connect' listener
+            if(err)
+                output[config.name] = ["ERR",err];
+            else{
+                    client.on('data', function(data) 
+                    {
+                        
+                        //console.log(config.name); Config.name referring to Blogs here for some reason
+                        output["Direct Connect ++"]=['OK', 200]; 
+                      client.end();
+                    });
+                }
+             cb();
+        });
+        
     }
 }
 
